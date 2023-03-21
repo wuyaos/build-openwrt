91a31bff6e9f205366115f65d45eee63cbfe2c8f
wuyaos <wufeifeng_hust@163.com>
2023-03-21 20:00:54 +0800

    更新

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -4,10 +4,11 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+
 # 后台IP设置
-export Ipv4_ipaddr="192.168.123.1"          # 修改openwrt后台地址(填0为关闭)
+export Ipv4_ipaddr="192.168.123.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
-export Op_name="Lienol"                     # 修改主机名称为OpenWrt-123(填0为不作修改)
+export Op_name="0"                # 修改主机名称为OpenWrt-123(填0为不作修改)
 
 # 默认主题设置
 export Mandatory_theme="argon"              # 将bootstrap替换您需要的主题为必选主题(可自行更改您要的,源码要带此主题就行,填写名称也要写对) (填写主题名称,填0为不作修改)
@@ -79,12 +80,12 @@ sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
 sed -i 's/"TTYD 终端"/"TTYD"/g' `egrep "TTYD 终端" -rl ./`
 sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
 sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
-sed -i 's/"管理权"/"更改密码"/g' `egrep "管理权" -rl ./`
-sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
+sed -i 's/"管理权"/"更改改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"NetData"/"实时监控"/g' `egrep "NetData" -rl ./`
-# sed -i 's/"DDNSTO 远程控制"/"DDNSTO"/g' `egrep "DDNSTO 远程控制" -rl ./`
-# sed -i 's/"wolplus"/"网络唤醒"/g' `egrep "wolplus" -rl ./`
-# sed -i 's/"IP/MAC Binding"/"IP/MAC绑定"/g' `egrep "IP/MAC Binding" -rl ./`
+sed -i 's/"DDNSTO 远程控制"/"DDNSTO"/g' `egrep "DDNSTO 远程控制" -rl ./`
+sed -i 's/"wolplus"/"网络唤醒"/g' `egrep "wolplus" -rl ./`
+sed -i 's/"IP/MAC Binding"/"IP/MAC绑定"/g' `egrep "IP/MAC Binding" -rl ./`
+
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
 cat >"$CLEAR_PATH" <<-EOF
